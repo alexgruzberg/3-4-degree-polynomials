accf487abdb52b945bc9700a862e518d3b6547ce
Михаил <mike1024b@mail.ru>
2022-04-29 18:16:53 +0300

    error estimation has been executed

--- 3-4-degree-polynomials/main.cpp
+++ 3-4-degree-polynomials/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include "polynomials.h"
 #include <random>
 #include <chrono>
+#include <numeric>
 using namespace std;
 
 //setting the range for the RNG//
@@ -20,26 +21,32 @@ int main()
     std::mt19937::result_type const seedval = dur;
     rng.seed(seedval);
 
-    // do something //
+    // estimating the error //
     vector<float> random_roots(3);
     vector<float> estimated_roots(3);
-    float error = 0;
-    for (int i = 0; i < 10; ++i)
+    vector<float> error_est_sum(10000);
+    vector<float> error_est_max(10000);
+    for (int i = 0; i < 10000; ++i)
     {
         for (int j = 0; j < 3; ++j)
             random_roots[j] = udist(rng) / 1e8;
         third_degree_polynomial P(random_roots);
-        P.info();
-        estimated_roots = P.tomas_co();
+        //P.info();
+        estimated_roots = P.cardon();
         sort(estimated_roots.begin(), estimated_roots.end());
-        for (auto v : estimated_roots) std::cout << v << " "; std::cout << std::endl;
-        std::cout << "error_max " << P.error_est_max(estimated_roots) << std::endl;
+        error_est_sum[i] = P.error_est_sum(estimated_roots);
+        error_est_max[i] = P.error_est_max(estimated_roots);
     }
-    /*vector<float> random_roots({3,2,4});
-    third_degree_polynomial P(random_roots);
-    P.info();
-    vector<float> estimated_roots = P.cardon();
-    sort(estimated_roots.begin(), estimated_roots.end());
-    for (auto v : estimated_roots) std::cout << v << " "; std::cout << std::endl;*/
+    sort(error_est_sum.begin(), error_est_sum.end());
+    cout << "Error estimation sum| x-x'|" << std::endl;
+    cout << "Min : " << error_est_sum[0] << std::endl;
+    cout << "Max : " << error_est_sum[9999] << std::endl;
+    cout << "Median : " << error_est_sum[4999] << std::endl << std::endl;
 
+    sort(error_est_max.begin(), error_est_max.end());
+    cout << "Error estimation sum( | x-x'| / max(x,x') )" << std::endl;
+    cout << "Min : " << error_est_max[0] << std::endl;
+    cout << "Max : " << error_est_max[9999] << std::endl;
+    cout << "Median : " << error_est_max[4999] << std::endl;
+    
 }
