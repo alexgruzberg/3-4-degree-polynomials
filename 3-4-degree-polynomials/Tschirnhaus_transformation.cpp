832cfc8723cfd3d89e8c7e444ec2b42201d68219
alexgruzberg <72379785+alexgruzberg@users.noreply.github.com>
2022-06-13 02:02:32 +0300

    encoding change

--- 3-4-degree-polynomials/Tschirnhaus_transformation.cpp
+++ 3-4-degree-polynomials/Tschirnhaus_transformation.cpp
@@ -1,11 +1,11 @@
 /*
-Здесь представлена реализация метода [преобразования] Чирнгауза для нахождения 
-корней кубического уравнения, записанного в виде x^3 + bx^2 + cx + d = 0.
+Р—РґРµСЃСЊ РїСЂРµРґСЃС‚Р°РІР»РµРЅР° СЂРµР°Р»РёР·Р°С†РёСЏ РјРµС‚РѕРґР° [РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёСЏ] Р§РёСЂРЅРіР°СѓР·Р° РґР»СЏ РЅР°С…РѕР¶РґРµРЅРёСЏ 
+РєРѕСЂРЅРµР№ РєСѓР±РёС‡РµСЃРєРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ, Р·Р°РїРёСЃР°РЅРЅРѕРіРѕ РІ РІРёРґРµ x^3 + bx^2 + cx + d = 0.
 
-Программа адаптирована под многочлены, содержащие комплексные коэффициенты,
-поэтому класс third_degree_polynomial не используется ввиду отсутствия возможности работать с комплексными коэффициентами.
+РџСЂРѕРіСЂР°РјРјР° Р°РґР°РїС‚РёСЂРѕРІР°РЅР° РїРѕРґ РјРЅРѕРіРѕС‡Р»РµРЅС‹, СЃРѕРґРµСЂР¶Р°С‰РёРµ РєРѕРјРїР»РµРєСЃРЅС‹Рµ РєРѕСЌС„С„РёС†РёРµРЅС‚С‹,
+РїРѕСЌС‚РѕРјСѓ РєР»Р°СЃСЃ third_degree_polynomial РЅРµ РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ РІРІРёРґСѓ РѕС‚СЃСѓС‚СЃС‚РІРёСЏ РІРѕР·РјРѕР¶РЅРѕСЃС‚Рё СЂР°Р±РѕС‚Р°С‚СЊ СЃ РєРѕРјРїР»РµРєСЃРЅС‹РјРё РєРѕСЌС„С„РёС†РёРµРЅС‚Р°РјРё.
 
-Даны краткие комментарии к функциям (полное описание см. в документации).
+Р”Р°РЅС‹ РєСЂР°С‚РєРёРµ РєРѕРјРјРµРЅС‚Р°СЂРёРё Рє С„СѓРЅРєС†РёСЏРј (РїРѕР»РЅРѕРµ РѕРїРёСЃР°РЅРёРµ СЃРј. РІ РґРѕРєСѓРјРµРЅС‚Р°С†РёРё).
 */
 
 #define _USE_MATH_DEFINES
@@ -20,19 +20,19 @@
 #include "exceptions.h"
 
 using namespace std;
-//функция для нахождения всех [трёх] значений кубического корня
+//С„СѓРЅРєС†РёСЏ РґР»СЏ РЅР°С…РѕР¶РґРµРЅРёСЏ РІСЃРµС… [С‚СЂС‘С…] Р·РЅР°С‡РµРЅРёР№ РєСѓР±РёС‡РµСЃРєРѕРіРѕ РєРѕСЂРЅСЏ
 template<typename T>
 vector<complex<T>> cubic_roots(complex<T> z)
 {
     complex<double> one_third = { 1.0 / 3.0, 0 };
 
-    complex<double> phi = arg(z);   //аргумент комплексного числа
-    complex<double> module = abs(z);    //модуль комплексного числа
+    complex<double> phi = arg(z);   //Р°СЂРіСѓРјРµРЅС‚ РєРѕРјРїР»РµРєСЃРЅРѕРіРѕ С‡РёСЃР»Р°
+    complex<double> module = abs(z);    //РјРѕРґСѓР»СЊ РєРѕРјРїР»РµРєСЃРЅРѕРіРѕ С‡РёСЃР»Р°
 
-    complex<double> cubic_root_module = pow(module, one_third);  //кубический корень из модуля комплексного числа
+    complex<double> cubic_root_module = pow(module, one_third);  //РєСѓР±РёС‡РµСЃРєРёР№ РєРѕСЂРµРЅСЊ РёР· РјРѕРґСѓР»СЏ РєРѕРјРїР»РµРєСЃРЅРѕРіРѕ С‡РёСЃР»Р°
 
     vector<complex<T>> roots;
-    for (int n = 0; n <= 2; n++)    //вычисление значений корня
+    for (int n = 0; n <= 2; n++)    //РІС‹С‡РёСЃР»РµРЅРёРµ Р·РЅР°С‡РµРЅРёР№ РєРѕСЂРЅСЏ
     {
         double p = 2 * M_PI * n;
 
@@ -43,38 +43,38 @@ vector<complex<T>> cubic_roots(complex<T> z)
     return roots;
 }
 
-//функция для нахождения корней квадратного уравнения со старшим коэффициентом a = 1
+//С„СѓРЅРєС†РёСЏ РґР»СЏ РЅР°С…РѕР¶РґРµРЅРёСЏ РєРѕСЂРЅРµР№ РєРІР°РґСЂР°С‚РЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ СЃРѕ СЃС‚Р°СЂС€РёРј РєРѕСЌС„С„РёС†РёРµРЅС‚РѕРј a = 1
 template<typename T>
 vector<complex<T>> roots_of_square_poly(vector<complex<T>> s)
 {
     complex<double> one_second = { 0.5, 0 };
-    complex<double> D = s[0] * s[0] - complex<double>{ 4.0, 0 } * s[1]; //дискриминант
-    complex<double> rootD = sqrt(D);    //корень из дискриминанта
+    complex<double> D = s[0] * s[0] - complex<double>{ 4.0, 0 } * s[1]; //РґРёСЃРєСЂРёРјРёРЅР°РЅС‚
+    complex<double> rootD = sqrt(D);    //РєРѕСЂРµРЅСЊ РёР· РґРёСЃРєСЂРёРјРёРЅР°РЅС‚Р°
     complex<T> x_1 = (-s[0] + rootD) * one_second;
     complex<T> x_2 = (-s[0] - rootD) * one_second;
     return { x_1, x_2 };
 }
 
-//функция для приведения общего вида многочлена 3-ей степени [x^3 + bx^2 + cx + d] к виду t^3 + pt + q
+//С„СѓРЅРєС†РёСЏ РґР»СЏ РїСЂРёРІРµРґРµРЅРёСЏ РѕР±С‰РµРіРѕ РІРёРґР° РјРЅРѕРіРѕС‡Р»РµРЅР° 3-РµР№ СЃС‚РµРїРµРЅРё [x^3 + bx^2 + cx + d] Рє РІРёРґСѓ t^3 + pt + q
 template<typename T>
 vector<complex<T>> canonical_reduction(vector<complex<T>> P)
 {
     complex<double> one_third = { 1.0 / 3.0, 0 };
     complex<double> one_twentyseventh = { 1.0 / 27.0, 0 };
    
-    complex<double> d = P[2], c = P[1], b = P[0];   //коэффициенты
+    complex<double> d = P[2], c = P[1], b = P[0];   //РєРѕСЌС„С„РёС†РёРµРЅС‚С‹
 
-    complex<T> p = (complex<double>{ 3, 0 } * c - b * b)* one_third;    //новые коэффициенты
+    complex<T> p = (complex<double>{ 3, 0 } * c - b * b)* one_third;    //РЅРѕРІС‹Рµ РєРѕСЌС„С„РёС†РёРµРЅС‚С‹
     complex<T> q = (complex<double>{27, 0} * d - complex<double>{9, 0} * b * c + complex<double>{2, 0} * b * b * b) * one_twentyseventh;
 
     return { p,q };
 }
 
-//Преобразование Чирнгауза
+//РџСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёРµ Р§РёСЂРЅРіР°СѓР·Р°
 template<typename T>
 vector<complex<T>> Tschirnhaus_transformation(vector<complex<T>> F)
 {
-    if ((F[1] == complex<double>{0, 0}) && (F[2] == complex<double>{0, 0}))     //быстрый подсчёт корней при c = d = 0
+    if ((F[1] == complex<double>{0, 0}) && (F[2] == complex<double>{0, 0}))     //Р±С‹СЃС‚СЂС‹Р№ РїРѕРґСЃС‡С‘С‚ РєРѕСЂРЅРµР№ РїСЂРё c = d = 0
     {
         return { complex<T>{0, 0},complex<T>{0, 0},-F[0] };
     }
@@ -82,7 +82,7 @@ vector<complex<T>> Tschirnhaus_transformation(vector<complex<T>> F)
     complex<double> two_third = { 2.0 / 3.0, 0 };
     complex<double> one_twentyseventh = { 1.0 / 27.0, 0 };
 
-    vector<complex<double>> reduct = canonical_reduction(F);    //приведение к упрощённому виду [t^3 + pt + q]
+    vector<complex<double>> reduct = canonical_reduction(F);    //РїСЂРёРІРµРґРµРЅРёРµ Рє СѓРїСЂРѕС‰С‘РЅРЅРѕРјСѓ РІРёРґСѓ [t^3 + pt + q]
     complex<double> p =  reduct[0];
     complex<double> q = reduct[1];
 
@@ -91,7 +91,7 @@ vector<complex<T>> Tschirnhaus_transformation(vector<complex<T>> F)
     complex<double> pp = p * p;
     complex<double> ppp = p * p * p;
     complex<double> reverse_p = complex<double>{ 1,0 } / p;
-    if ((isinf(real(reverse_p)) || isnan(real(reverse_p))) && (isinf(imag(reverse_p)) || isnan(imag(reverse_p))))   //проверка на ноль в знаменателе [если да - поиск корней для t^3 + q = 0]
+    if ((isinf(real(reverse_p)) || isnan(real(reverse_p))) && (isinf(imag(reverse_p)) || isnan(imag(reverse_p))))   //РїСЂРѕРІРµСЂРєР° РЅР° РЅРѕР»СЊ РІ Р·РЅР°РјРµРЅР°С‚РµР»Рµ [РµСЃР»Рё РґР° - РїРѕРёСЃРє РєРѕСЂРЅРµР№ РґР»СЏ t^3 + q = 0]
     {
         //throw division_by_zero();
         complex<double> y_cubic_0 = -q;
@@ -104,13 +104,13 @@ vector<complex<T>> Tschirnhaus_transformation(vector<complex<T>> F)
     }
     complex<double> reverse_pp = reverse_p* reverse_p;
 
-    vector<complex<double>> find_a = { complex<double>{ 3, 0 }*q* reverse_p, -one_third * p };  //коэффициенты для квадратного уравнения
-    vector<complex<double>> a = roots_of_square_poly(find_a);   //корни квадратного уравнения
+    vector<complex<double>> find_a = { complex<double>{ 3, 0 }*q* reverse_p, -one_third * p };  //РєРѕСЌС„С„РёС†РёРµРЅС‚С‹ РґР»СЏ РєРІР°РґСЂР°С‚РЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ
+    vector<complex<double>> a = roots_of_square_poly(find_a);   //РєРѕСЂРЅРё РєРІР°РґСЂР°С‚РЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ
     complex<double> b = two_third * p;
-    /*Далее в вычислениях участвует только один корень квадратного уравнения (пояснение см. в документации)*/
+    /*Р”Р°Р»РµРµ РІ РІС‹С‡РёСЃР»РµРЅРёСЏС… СѓС‡Р°СЃС‚РІСѓРµС‚ С‚РѕР»СЊРєРѕ РѕРґРёРЅ РєРѕСЂРµРЅСЊ РєРІР°РґСЂР°С‚РЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ (РїРѕСЏСЃРЅРµРЅРёРµ СЃРј. РІ РґРѕРєСѓРјРµРЅС‚Р°С†РёРё)*/
     complex<double> y_cubic_1 = complex<double>{ 9, 0 } * qqq * a[0] * reverse_pp + complex<double>{ 4, 0 }*one_third* a[0] * p * q - complex<double>{ 8, 0 }*one_twentyseventh* ppp - complex<double>{ 2, 0 } * qq;
 
-    vector<complex<double>> y1 = cubic_roots(y_cubic_1);    //проверка кубических корней из y1 на нулевое значение
+    vector<complex<double>> y1 = cubic_roots(y_cubic_1);    //РїСЂРѕРІРµСЂРєР° РєСѓР±РёС‡РµСЃРєРёС… РєРѕСЂРЅРµР№ РёР· y1 РЅР° РЅСѓР»РµРІРѕРµ Р·РЅР°С‡РµРЅРёРµ
     if (y1 == vector<complex<double>>{0, 0, 0})
     {
         vector<complex<double>> sq_poly_coefs0 = { a[0], b };
@@ -121,7 +121,7 @@ vector<complex<T>> Tschirnhaus_transformation(vector<complex<T>> F)
     
     double eps = 1e-3;
     vector<complex<double>> six_roots;  
-    for (int i = 0; i <= 2; i++)    //вычисление шести корней 
+    for (int i = 0; i <= 2; i++)    //РІС‹С‡РёСЃР»РµРЅРёРµ С€РµСЃС‚Рё РєРѕСЂРЅРµР№
     {
         complex<double> sum_b_y1 = b + y1[i];
         vector<complex<double>> sq_poly_coefs1 = { a[0], sum_b_y1 };
@@ -131,7 +131,7 @@ vector<complex<T>> Tschirnhaus_transformation(vector<complex<T>> F)
     }
     vector<complex<T>> final_roots;
     complex<double> d0 = F[2], c0 = F[1], b0 = F[0];
-    for (int j = 0; j <= 5; j++)    //отбрасывание посторонних корней
+    for (int j = 0; j <= 5; j++)    //РѕС‚Р±СЂР°СЃС‹РІР°РЅРёРµ РїРѕСЃС‚РѕСЂРѕРЅРЅРёС… РєРѕСЂРЅРµР№
     {
         complex<double> value = six_roots[j];
         complex<double> x_square = value * value;
@@ -147,7 +147,7 @@ vector<complex<T>> Tschirnhaus_transformation(vector<complex<T>> F)
 }
 
 template<typename T>
-void print(vector<complex<T>> v)    //вывод в консоль
+void print(vector<complex<T>> v)    //РІС‹РІРѕРґ РІ РєРѕРЅСЃРѕР»СЊ
 {
     for (unsigned int k = 0; k < v.size(); k++)
     {
